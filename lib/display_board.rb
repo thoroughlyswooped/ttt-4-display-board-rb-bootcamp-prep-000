506c511571a611290ad31ecda11533a96813724e
Ezra T Huffman <joshooha@gmail.com>
2018-09-27 14:41:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,8 @@
 # out the current state.
 def display_board(array)
   puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------------------------------------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------------------------------------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
+end
